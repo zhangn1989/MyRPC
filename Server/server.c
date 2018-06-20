084fe0ce0692d64de98426911cec4e3d6f65fa42
zhangn1989 <mumufan@foxmail.com>
2018-06-20 18:35:14 +0800

    poll mould

--- Server/server.c
+++ Server/server.c
@@ -13,11 +13,13 @@
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
 #include <arpa/inet.h>
+#include <poll.h>
 
 #include "public_head.h"
 #include "fileio.h"
 
 #define LISTEN_BACKLOG 50
+#define MAX_CLIENT	1024
 
 static ssize_t handle_request(int acceptfd)
 {
@@ -43,12 +45,20 @@ static ssize_t handle_request(int acceptfd)
 
 int main(int argc, char ** argv)
 {
+	int i = 0;
     int sockfd = 0;
     int acceptfd = 0;
     socklen_t client_addr_len = 0;
     struct sockaddr_in server_addr, client_addr;
 
     char client_ip[16] = { 0 };
+
+	int ready;
+	int clientlen = 0;
+	struct pollfd clientfd[MAX_CLIENT];
+
+	for (i = 0; i < MAX_CLIENT; ++i)
+		clientfd[i].fd = -1;
 
     memset(&server_addr, 0, sizeof(server_addr));
     memset(&client_addr, 0, sizeof(client_addr));
@@ -72,21 +82,62 @@ int main(int argc, char ** argv)
         close(sockfd);
         handle_error("listen");
     }
+
+	clientfd[0].fd = sockfd;
+	clientfd[0].events = POLLRDNORM;
+	clientlen++;
 	
     while(1)
     {
-        client_addr_len = sizeof(client_addr);
-        if((acceptfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
-        {
-			handle_warning("accept");
+		ready = poll(clientfd, clientlen, 0);
+		if (ready < 0)
+		{
+			if(errno == EINTR)
+				continue;
+
+			close(sockfd);
+			handle_error("poll");
+		}
+		else if (ready == 0)
+		{
 			continue;
-        }
-       
-        memset(client_ip, 0, sizeof(client_ip));
-        inet_ntop(AF_INET,&client_addr.sin_addr,client_ip,sizeof(client_ip)); 
-        printf("client:%s:%d\n",client_ip,ntohs(client_addr.sin_port));
+		}
 
+		if (clientfd[0].revents & POLLRDNORM)
+		{
+			client_addr_len = sizeof(client_addr);
+			if ((acceptfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
+			{
+				handle_warning("accept");
+				continue;
+			}
 
+			memset(client_ip, 0, sizeof(client_ip));
+			inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+			printf("client:%s:%d\n", client_ip, ntohs(client_addr.sin_port));
+
+			clientfd[clientlen].fd = acceptfd;
+			clientfd[clientlen].events = POLLRDNORM;
+			clientlen++;
+
+			if (ready == 1)
+				continue;
+		}
+        
+		for (i = 0; i < clientlen; ++i)
+		{
+			if (clientfd[i].fd < 0)
+				continue;
+
+			if (clientfd[i].revents & POLLRDNORM)
+			{
+				if (handle_request(clientfd[i].fd) <= 0)
+				{
+					close(clientfd[i].fd);
+					clientfd[i].fd = -1;
+				}
+			}
+		}
     }
     
     close(sockfd);
