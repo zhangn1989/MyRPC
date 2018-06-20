204744d7eb122cc624ab5a2d8e0b7726f2ec15f9
zhangn1989 <mumufan@foxmail.com>
2018-06-20 17:34:20 +0800

    select mould

--- Server/server.c
+++ Server/server.c
@@ -19,39 +19,43 @@
 
 #define LISTEN_BACKLOG 50
 
-static void handle_request(int acceptfd)
+static ssize_t handle_request(int acceptfd)
 {
-    int i = 0;
     ssize_t readret = 0;
     char read_buff[256] = { 0 };
     char write_buff[256] = { 0 };
    
-    for(i = 0; i < 10; ++i)
-    {
-        memset(read_buff, 0, sizeof(read_buff));
-        readret = read(acceptfd, read_buff, sizeof(read_buff));
-        if(readret == 0)
-            break;
+   
+	memset(read_buff, 0, sizeof(read_buff));
+	readret = read(acceptfd, read_buff, sizeof(read_buff));
+	if (readret == 0)
+		return readret;
 
-        printf("progress id:%d, recv message:%s\n", getpid(), read_buff);
+	printf("acceptfd:%d, recv message:%s\n", acceptfd, read_buff);
 
-        memset(write_buff, 0, sizeof(write_buff));
-        sprintf(write_buff, "This is server send message:%d", i);
-        write(acceptfd, write_buff, sizeof(write_buff));
-    }
+	memset(write_buff, 0, sizeof(write_buff));
+	sprintf(write_buff, "This is server send message");
+	write(acceptfd, write_buff, sizeof(write_buff));
+ 
     printf("\n");
-    close(acceptfd);
-    return;
+    return readret;
 }
 
 int main(int argc, char ** argv)
 {
-    int sockfd = 0;
-    int acceptfd = 0;
+    int sockfd = -1;
+    int acceptfd = -1;
     socklen_t client_addr_len = 0;
     struct sockaddr_in server_addr, client_addr;
 
     char client_ip[16] = { 0 };
+
+	int clientfd[FD_SETSIZE];
+	int i = 0, client_index = 0;
+	int ready = -1, nfds = -1;
+	fd_set rset;
+	struct timeval timeout = { 0, 0 };
+	FD_ZERO(&rset);
 
     memset(&server_addr, 0, sizeof(server_addr));
     memset(&client_addr, 0, sizeof(client_addr));
@@ -75,21 +79,85 @@ int main(int argc, char ** argv)
         close(sockfd);
         handle_error("listen");
     }
+
+	for (i = 0; i < FD_SETSIZE; ++i)
+		clientfd[i] = -1;
+
+	int fd_stdin = fileno(stdin);
+	nfds = fd_stdin > sockfd ? fd_stdin : sockfd;
 	
     while(1)
     {
-        client_addr_len = sizeof(client_addr);
-        if((acceptfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
-        {
-			handle_warning("accept");
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 0;
+		FD_SET(sockfd, &rset);
+		for (i = 0; i < client_index; ++i)
+		{
+			if( clientfd[i] < 0)
+				continue;
+
+			FD_SET(clientfd[i], &rset);
+		}
+
+		ready = select(nfds + 1, &rset, NULL, NULL, &timeout);
+		if (ready < 0)
+		{
+			if (errno == EINTR)
+				continue;
+
+			close(sockfd);
+			handle_error("select");
+		}
+		else if (ready == 0)
+		{
 			continue;
-        }
-       
-        memset(client_ip, 0, sizeof(client_ip));
-        inet_ntop(AF_INET,&client_addr.sin_addr,client_ip,sizeof(client_ip)); 
-        printf("client:%s:%d\n",client_ip,ntohs(client_addr.sin_port));
+		}
 
+		if (FD_ISSET(sockfd, &rset))
+		{
+			client_addr_len = sizeof(client_addr);
+			if ((acceptfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
+			{
+				handle_warning("accept");
+				continue;
+			}
 
+			memset(client_ip, 0, sizeof(client_ip));
+			inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+			printf("client:%s:%d\n", client_ip, ntohs(client_addr.sin_port));
+
+			clientfd[client_index++] = acceptfd;
+
+			FD_SET(acceptfd, &rset);
+			if (acceptfd > nfds)
+				nfds = acceptfd;
+			if (ready == 1)
+				continue;
+		}
+
+		for (i = 0; i < client_index; ++i)
+		{
+			if (clientfd[i] < 0)
+				continue;
+
+			if (FD_ISSET(clientfd[i], &rset))
+			{
+				if (handle_request(clientfd[i]) <= 0)
+				{
+					FD_CLR(clientfd[i], &rset);
+					close(clientfd[i]);
+					clientfd[i] = -1;
+				}
+			}
+		}
+
+// 		for (i = 0; i < client_index; ++i)
+// 		{
+// 			if (FD_ISSET(clientfd[i], &writefds))
+// 			{
+// 
+// 			}
+// 		}
     }
     
     close(sockfd);
